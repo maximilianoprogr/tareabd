7d7f5fb077d0f27c810ba7c8b2ad8fafb9783c47
smfabiann <smfabiann@gmail.com>
2025-05-12 18:54:24 -0400

    a

--- php/register.php
+++ php/register.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 include('../php/db_connection.php'); // Asegúrate de que este archivo define correctamente la variable $conn
 
 $message = ""; // Variable para almacenar mensajes

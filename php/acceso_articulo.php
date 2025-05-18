74f01c31d210e54d21da21a5d06fd4eb6f016374
maximilianoprogr <maximilianov.yanez@gmail.com>
2025-05-18 16:04:51 -0400

    |1

--- php/acceso_articulo.php
+++ php/acceso_articulo.php
@@ -95,7 +95,7 @@ if ($articulo_seleccionado) {
                     <?php foreach ($revisores as $revisor): ?>
                         <a href="revisiones.php?revision=<?php echo htmlspecialchars($revisor); ?>" class="button">R<?php echo htmlspecialchars($revisor); ?> Consultar</a>
                         <div style="margin-top: 5px;">
-                            <a href="opinar.php?revision=<?php echo htmlspecialchars($revisor); ?>" class="button">R<?php echo htmlspecialchars($revisor); ?> Opinar</a>
+                            <a href="opinar.php?revision=<?php echo htmlspecialchars($articulo_seleccionado); ?>" class="button">R<?php echo htmlspecialchars($revisor); ?> Opinar</a>
                         </div>
                     <?php endforeach; ?>
                 <?php else: ?>
